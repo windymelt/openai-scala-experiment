d5556502f72951144979c501f7a49e247788c829
Windymelt <windymelt@3qe.us>
2023-03-21 08:11:45 +0900

    Thus, we wrote simple client

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,5 +4,17 @@ import cats.effect.IOApp.Simple
 import cats.effect.IO
 
 object Main extends Simple {
-  def run: IO[Unit] = IO.println("IO App")
+  import org.http4s.ember.client.EmberClientBuilder
+  import org.http4s.client.Client
+  import cats.implicits._
+  def run: IO[Unit] = {
+    // prepare client resource
+    val clientResource = EmberClientBuilder.default[IO].build
+
+    // use client resource
+    clientResource.use { c =>
+      // expect returns IO[String] and we pass it to IO.println
+      c.expect[String]("https://www.3qe.us/") >>= IO.println
+    }
+  }
 }
